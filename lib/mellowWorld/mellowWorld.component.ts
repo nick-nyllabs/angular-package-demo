882268d6749c739803c6496a5815ceb6fba748a8
Nick Palenchar <nick@nyllabs.tech>
2017-03-31 14:59:18 -0400

    commit before changing entry file

---
+++ lib/mellowWorld/mellowWorld.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'mellow-world',
+    templateUrl: 'mellowWorld.template.html',
+    styleUrls: ['mellowWorld.css']
+})
+
+export class MellowWorldComponent { }
